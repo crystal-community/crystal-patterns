54a0efaba16435a6f9a3f3d755b630d251ca016e
Vitalii Elenhaupt <velenhaupt@gmail.com>
2016-08-01 11:49:54 +0300

    Rename compile to build

--- rebuild.sh
+++ rebuild.sh
@@ -4,7 +4,7 @@ ret=0
 
 for src in creational/*.cr structural/*.cr behavioral/*.cr
 do
-  (set -x; crystal compile --no-codegen $src) || ret=$?
+  (set -x; crystal build --no-codegen $src) || ret=$?
 done
 
 exit $ret
